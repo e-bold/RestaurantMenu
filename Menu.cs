42c256dc83938a49e90dd60c02ecb5a31446f201
e-bold <enkhmunkh@att.net>
2024-05-26 09:56:24 -0500

    Initital commit of Chapter4 studio

---
+++ Menu.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantMenu 
+{
+    public class Menu
+    {
+    public List<MenuItem> MenuItems {get; set;} = [];
+    public DateTime LastUpdated {get; set;} = DateTime.Now;
+    }
+}
+
